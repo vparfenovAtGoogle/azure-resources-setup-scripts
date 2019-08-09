0a564747c0449f54e175ca535f9eb35e2fa126c4
vparfenovAtGoogle <53707044+vparfenovAtGoogle@users.noreply.github.com>
2019-08-09 11:08:38 -0400

    Create SetupGitNodeJS-Ubuntu.sh

---
+++ SetupGitNodeJS-Ubuntu.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt-get install git
+sudo apt install npm
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install express --save
+npm install hbs --save
+npm install uuid --save
+nodejs bin/www
+git clone https://github.com/vparfenovAtGoogle/TestGitHubAPIs.git
+git pull https://github.com/vparfenovAtGoogle/TestGitHubAPIs.git
