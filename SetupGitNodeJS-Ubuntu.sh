b195bbc946b3fd49606293f4dda40749f8940522
vparfenovAtGoogle <53707044+vparfenovAtGoogle@users.noreply.github.com>
2019-08-15 09:33:12 -0400

    Added docker installation

--- SetupGitNodeJS-Ubuntu.sh
+++ SetupGitNodeJS-Ubuntu.sh
@@ -1,12 +1,34 @@
+# Updating package registry index
 sudo apt update
+#sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg-agent     software-properties-common
+# Installing git client
 sudo apt-get install git
+# Installing Node.JS
 sudo apt install npm
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
+# Installing Node.JS packages
 npm install express --save
 npm install hbs --save
 npm install uuid --save
-nodejs bin/www
+# Cloning application from github
 git clone https://github.com/vparfenovAtGoogle/TestGitHubAPIs.git
 git pull https://github.com/vparfenovAtGoogle/TestGitHubAPIs.git
+# Starting Node.JS application
+nodejs bin/www
+# Testing Node.JS application by issuing a simple HTTP request
 curl http://localhost:3000
+# Installing Docker community edition
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install docker-ce
+# Adding user to docker group to fix access: change vparfenov to your username
+sudo usermod -a -G docker vparfenov
+# Restart docker service just in case
+sudo service docker restart
+# Use from another terminal
+docker ps
+docker images
